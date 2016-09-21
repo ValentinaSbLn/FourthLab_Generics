6c308b75998a37e8737626036fe0c760d67d0e08
ValentinaSbLn <Aliara@yandex.ru>
2016-09-21 14:03:26 +0300

    Solution

--- src/generics/CountMapIml.java
+++ src/generics/CountMapIml.java
@@ -6,8 +6,8 @@ import java.util.Map;
 /**
  * Created by Valentina on 20.09.2016.
  */
-public class CountMapIml<T> implements CountMap<T>{
-    private Map<T, Integer>items=new HashMap<>();
+public class CountMapIml<T> implements CountMap<T> {
+    private Map<T, Integer> items = new HashMap<>();
 
     @Override
     public void add(T t) {
@@ -22,7 +22,7 @@ public class CountMapIml<T> implements CountMap<T>{
 
     @Override
     public int remove(T t) {
-        int count=this.getCount(t);
+        int count = this.getCount(t);
         this.items.remove(t);
         return count;
     }
@@ -34,30 +34,26 @@ public class CountMapIml<T> implements CountMap<T>{
 
     @Override
     public void addAll(CountMap<? extends T> countMap) {
-        Map<? extends T,Integer> map = countMap.toMap();
+        Map<? extends T, Integer> map = countMap.toMap();
         for (T t : map.keySet()) {
             items.put(t, (items.containsKey(t)) ? items.get(t) + map.get(t) : 1);
         }
-
-        for (T s : items.keySet())
-            System.out.println("addAll "+s + ": " + items.get(s));
     }
 
     @Override
     public Map<T, Integer> toMap() {
-        Map<T, Integer> map=new HashMap<T, Integer>();
-                for (T t : items.keySet()) {
-            map.put(t,  items.get(t));
+        Map<T, Integer> map = new HashMap<T, Integer>();
+        for (T t : items.keySet()) {
+            map.put(t, items.get(t));
         }
         return map;
     }
 
     @Override
-    public void toMap(Map<? super T, Integer>destination) {
+    public void toMap(Map<? super T, Integer> destination) {
         destination.clear();
         for (T t : items.keySet()) {
-            destination.put(t,  items.get(t));
+            destination.put(t, items.get(t));
         }
-
-     }
+    }
 }
