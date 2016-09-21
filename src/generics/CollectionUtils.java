d72d27dfaeea36e3dff3ddf6bbbebc63371e1692
ValentinaSbLn <Aliara@yandex.ru>
2016-09-21 21:03:05 +0300

    Change CollectionUtils

--- src/generics/CollectionUtils.java
+++ src/generics/CollectionUtils.java
@@ -6,140 +6,180 @@ import java.util.*;
  * Created by Valentina on 21.09.2016.
  */
 public class CollectionUtils {
-    public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
+
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
 
     public static <T> List<T> newArrayList() {
-            return new ArrayList<>();
+        return new ArrayList<T>();
     }
 
-    public static <T>int indexOf(List<? extends T> source, T t) {
+    public static <T> int indexOf(List<? extends T> source, T t) {
         return source.indexOf(t);
     }
-//
-    public static <T>List <T>limit(List<? extends T> source, int size) {
-        List<T> list=new ArrayList<>();
-        int limit=size<=source.size() ? size : source.size();
-            for (int i = 0; i < limit; i++)
-                list.add(source.get(i));
-            return list;
 
+    public static <T> List<T> limit(List<? extends T> source, int size) {
+        List<T> list = new ArrayList<>();
+        int limit = size <= source.size() ? size : source.size();
+        for (int i = 0; i < limit; i++)
+            list.add(source.get(i));
+        return list;
     }
-//
-    public static <T>void add(List<? super T> source, T t) {
+
+    public static <T> void add(List<? super T> source, T t) {
         source.add(t);
     }
 
-    public static <T>void removeAll(List <? super T> removeFrom, List <? extends T> c2) {
-        for (int i=0; i<c2.size(); i++){
-            for (int j=0; j<removeFrom.size(); j++){
-                if (c2.get(i).equals(removeFrom.get(j))){
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
+        for (int i = 0; i < c2.size(); i++) {
+            for (int j = 0; j < removeFrom.size(); j++) {
+                if (c2.get(i).equals(removeFrom.get(j))) {
                     removeFrom.remove(j);
                 }
             }
         }
     }
-//
-    public static <T>boolean containsAll(List <? extends T> c1, List <? extends T> c2) {
-       return c1.containsAll(c2);
+
+    public static <T> boolean containsAll(List<? extends T> c1, List<? extends T> c2) {
+        return c1.containsAll(c2);
     }
-//
-    public static <T>boolean containsAny(List <? extends T> c1, List <? extends T> c2) {
-        for (T t: c2){
-           if (c1.contains(t))
-               return true;
+
+    public static <T> boolean containsAny(List<? extends T> c1, List<? extends T> c2) {
+        for (T t : c2) {
+            if (c1.contains(t))
+                return true;
         }
         return false;
     }
 
-    public static <T extends Number> List<T> range(List <? extends T>  list, T min, T max) {
+
+    public static <T extends Comparable<? super T>> List<T> range(List<T> list, T min, T max) {
+
         List<T> sortList = new ArrayList<T>();
         sortList.addAll(list);
-        Set<T>sort=new HashSet<>();
-        for (T t: list)
-        sort.add(t);
 
-        Collections.sort(list, new Comparator<T>() {
+        Collections.sort(sortList, new Comparator<T>() {
             public int compare(T o1, T o2) {
                 return o1.compareTo(o2);
             }
         });
+        if (min.compareTo(max) > 0) {
+            T temp = max;
+            max = min;
+            min = temp;
+        }
+        int minIndex = sortList.indexOf(min);
+        int maxIndex = sortList.indexOf(max);
 
+        if (minIndex == -1) {
+            for (int i = 0; i < sortList.size() - 1; i++) {
+                if (min.compareTo(sortList.get(0)) < 0) {
+                    minIndex = 0;
+                } else if (min.compareTo(sortList.get(i)) > 0 && min.compareTo(sortList.get(i + 1)) < 0)
+                    minIndex = i + 1;
+            }
+        }
+        if (maxIndex == -1) {
+            for (int i = 0; i < sortList.size() - 1; i++) {
+                if (max.compareTo(sortList.get(sortList.size() - 1)) > 0) {
+                    maxIndex = sortList.size() - 1;
+                } else if (max.compareTo(sortList.get(i)) > 0 && max.compareTo(sortList.get(i + 1)) < 0)
+                    maxIndex = i;
+            }
+        }
+
+        List<T> resultList = new ArrayList<T>();
+
+        if (minIndex != -1 && maxIndex != -1) {
+            for (int i = minIndex; i <= maxIndex; i++) {
+                resultList.add(sortList.get(i));
+            }
+        }
+        return resultList;
     }
 
 
-    public static <T>List <T>range(List <? extends T> list, T min, T max, Comparator <? super T>comparator) {
-
+    public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<? super T> comparator) {
 
         List<T> sortList = new ArrayList<T>();
         sortList.addAll(list);
+
         Collections.sort(sortList, comparator);
-        System.out.println("sortlist_range "+Arrays.toString(sortList.toArray()));
-        System.out.println("list_range "+Arrays.toString(list.toArray()));
-        int minIndex=sortList.indexOf(min);
+
+        if (comparator.compare(min, max) > 0) {
+            T temp = max;
+            max = min;
+            min = temp;
+        }
+        int minIndex = sortList.indexOf(min);
         int maxIndex = sortList.indexOf(max);
-        System.out.println(minIndex+" "+maxIndex);
+
+        if (minIndex == -1) {
+            for (int i = 0; i < sortList.size() - 1; i++) {
+                if (comparator.compare(min, sortList.get(0)) < 0) {
+                    minIndex = 0;
+                } else if (comparator.compare(min, sortList.get(i)) > 0 && comparator.compare(min, sortList.get(i + 1)) < 0)
+                    minIndex = i + 1;
+            }
+        }
+        if (maxIndex == -1) {
+            for (int i = 0; i < sortList.size() - 1; i++) {
+                if (comparator.compare(max, sortList.get(sortList.size() - 1)) > 0) {
+                    maxIndex = sortList.size() - 1;
+                } else if (comparator.compare(max, sortList.get(i)) > 0 && comparator.compare(max, sortList.get(i + 1)) < 0)
+                    maxIndex = i;
+            }
+        }
+
         List<T> resultList = new ArrayList<T>();
-        for (int i=minIndex; i<=maxIndex; i++){
-            resultList.add(sortList.get(i));
+        if (minIndex != -1 && maxIndex != -1) {
+            for (int i = minIndex; i <= maxIndex; i++) {
+                resultList.add(sortList.get(i));
+            }
         }
         return resultList;
-
     }
 
     public static void main(String[] args) {
-        CollectionUtils test=new CollectionUtils();
-//        List<String> s1=new ArrayList<>();
-//        s1.add("1");
-//        s1.add("1");
-//        List<String> s2=new ArrayList<>();
-//        test.addAll(s1,s2);
-//        for (String s: s2)
-//        System.out.println(s);
+        List<String> str1 = Arrays.asList("a","b");//a, b
 
-        List<Integer> list=test.newArrayList();
+        List<String> str2 = new ArrayList<>(); //c, d
+        str2.add("c");
+        str2.add("d");
 
-        list.add(1);
-        list.add(2);
-        list.add(5);
-        list.add(null);
+        addAll(str1, str2);
+        System.out.print("addAll(str1, str2):");
+        for (String s:str2)
+            System.out.print(s+" ");
 
-        List<Integer>list2;
-        list2=limit(list, 2);
+        System.out.print("\nIndexOf \"a\":"+indexOf(str2, "a"));
 
+        List<String>str3=limit(str2, 3); //c,d, a
+        System.out.print("\nlimit(str2, 3):");
+        for (String s:str3)
+            System.out.print(s+" ");
 
-//        System.out.println(test.indexOf(s1, "3"));
-//        System.out.println(s1.indexOf("3"));
-        add(list, 8);
+        add(str3,"e");
+        System.out.print("\nadd(str3,\"e\"):");
+        for (String s:str3)
+            System.out.print(s+" ");
 
-        System.out.println("Contains:"+containsAll(list, list2) );
+        removeAll(str3, str1); // из [c d a e]  удалить [a b]
+        System.out.print("\nremoveAll(str3, str1);");
+        for (String s:str3)
+            System.out.print(s+" ");
 
-        System.out.println(Arrays.toString(list2.toArray()));
-        System.out.println("before "+Arrays.toString(list.toArray()));
+        System.out.println("\n"+containsAll(str3, str2)); //str3: c d a, str2: c d a b
+        System.out.println(containsAny(str3, str2));
 
-        removeAll(list, list2);
-        System.out.println("after "+Arrays.toString(list.toArray()));
+        List<String> str4=range(str2, "a","c"); //str2: c d a b
+        System.out.print("range(str2, \"a\",\"c\"): ");
+        for (String s:str4)
+            System.out.print(s+" ");
 
-        System.out.println("Contains: "+containsAll(list, list2) );
-        add(list2, 6);
-        add(list, 4);
-        add(list, 7);
-        add(list, 2);
-        list.remove(null);
-        System.out.println("list "+Arrays.toString(list.toArray()));
-        System.out.println("list2 "+Arrays.toString(list2.toArray()));
-        System.out.println("ContainsAny: "+containsAny(list, list2));
-        Comparator<Integer> t= new Comparator<Integer>() {
-
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
-            }
-        };
-        List list3=range(Arrays.asList(8,1,3,5,6, 4), 3, 6,t) ;
-        System.out.println("list "+Arrays.toString(list.toArray()));
-        System.out.println("list3 "+Arrays.toString(list3.toArray()));
+        List<Integer>list=range(Arrays.asList(8,1,3,5,6, 4), -5, 5);
+        System.out.println("\n"+Arrays.toString(list.toArray()));
 
     }
 }
--- src/generics/TestCollUtils.java
+++
@@ -1,7 +0,0 @@
-package generics;
-
-/**
- * Created by Valentina on 21.09.2016.
- */
-public class TestCollUtils {
-}
